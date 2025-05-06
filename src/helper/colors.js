cbbe8798c3b374c759d3402fcce4f3b229a2cfb3
Your Name <you@example.com>
2025-05-06 20:18:20 -0300

    Adiciona alterações na mudança de cor do log condicionais em cadeia
    Signed-off-by: Your Name <you@example.com>

--- src/helper/colors.js
+++ src/helper/colors.js
@@ -1,5 +1,15 @@
-function defineColor(color) {
-    return `color: ${color};`;
+const colors = ["\x1b[31m", "\x1b[32m", "\x1b[33m"]
+
+function defineColor(message, color) {
+    if(color == "red") {
+        return colors[0] + message + "\x1b[0m";
+    } else if(color == "green") {
+        return colors[1] + message + "\x1b[0m";
+    } else if(color == "yellow") {
+        return colors[2] + message + "\x1b[0m";
+    } else {
+        return colors[0] + "Cor inválida..." + "\x1b[0m";
+    }
 }
 
 module.exports = defineColor;
