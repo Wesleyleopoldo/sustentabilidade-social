3941f5182c1bf6d0331bd87ab9f09d9606f47d9f
Your Name <you@example.com>
2025-05-06 19:45:50 -0300

    Corrige erro de exportação e importação
    Signed-off-by: Your Name <you@example.com>

--- src/helper/error.js
+++ src/helper/error.js
@@ -1,9 +1,11 @@
-const { defineColor } = require("./colors");
+const defineColor = require("./colors");
 
 async function useTry(fun, errorMessage) {
     try {
         await fun();
     } catch(error) {
-        console.log(`%cAlgo deu errado: ${errorMessage}`, defineColor);
+        console.log(`%cAlgo deu errado: ${errorMessage} (${error})`, defineColor("red"));
     }
 }
+
+module.exports = useTry;
