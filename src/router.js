542da0a631ca0eabb5a21d95c3bc7ec85e644574
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-07 16:31:49 -0300

    Adiciona novo endpoint para execução do metodo de criar adm
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/router.js
+++ src/router.js
@@ -3,5 +3,6 @@ const userController = require("./controllers/userController");
 const router = express.Router();
 
 router.post("/users", userController.createUser);
+router.post("/admin", userController.createAdmin);
 
 module.exports = router;
