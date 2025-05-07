c8002cd56d9e793a59ea640f9d4db3cae01c04ed
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-07 16:15:42 -0300

    Adiciona metodos com regras de negocios de criar usuário e admin
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/services/userservice.js
+++ src/services/userservice.js
@@ -1,6 +1,63 @@
-const useTry = require("../helper/error");
+const createUserDTO = require("../dtos/userDto");
+const { tryQuery, useTry } = require("../helper/error");
 const { User } = require("../resources/db");
 
 const createUser = async (body) => {
-    useTry()
+
+    const datas = {
+        picture_profile_url: body.picture_profile_url,
+        username: body.username,
+        email: body.email,
+        password: body.password,
+        role: "user"
+    }
+
+    const findUser = await User.findOne({
+        where: { email: datas.email }
+    });
+
+    if(findUser) {
+        throw new Error("Email já cadastrado!");
+    }
+
+    const newUser = await tryQuery("Erro ao tentar criar novo usuário", () => User.create(datas));
+    const newUserDto = createUserDTO({
+        picture_profile_url: newUser.picture_profile_url,
+        username: newUser.username,
+    });
+
+    return newUserDto;
+}
+
+const createAdmin = async (body) => {
+
+    const datas = {
+        picture_profile_url: body.picture_profile_url,
+        username: body.username,
+        email: body.email,
+        password: body.password,
+        role: "adm"
+    }
+
+    const findUser = await User.findOne({
+        where: { email: datas.email }
+    });
+
+    if(findUser) {
+        throw new Error("Email já cadastrado!");
+    }
+
+    const admin = await tryQuery("Erro ao tentar cadastrar administrador", () => User.create(datas));
+
+    const adminDto = createUserDTO({
+        picture_profile_url: admin.picture_profile_url,
+        username: admin.username
+    });
+
+    return adminDto;
+}
+
+module.exports = {
+    createUser,
+    createAdmin
 }
