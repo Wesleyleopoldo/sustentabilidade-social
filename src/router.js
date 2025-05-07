2781f8873464bf98c06988b449915eee6e500b8d
katiaR14 <katiadelima78@gmail.com>
2025-05-07 20:16:49 -0300

    Adiciona novos endpoints para listar usuários e buscar um usuário pelo id
    Signed-off-by: katiaR14 <katiadelima78@gmail.com>

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ const userController = require("./controllers/userController");
 const router = express.Router();
 
 router.post("/users", userController.createUser);
+router.get("/users", userController.indexAllUsers);
+router.get("/users/:id", userController.getUserById);
 router.post("/admin", userController.createAdmin);
 
 module.exports = router;
