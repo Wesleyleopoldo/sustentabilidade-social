f2ef6c5034c4ba93c1217aa9ab2947a0d97efd6d
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-07 16:32:22 -0300

    Adiciona teste de criação de usuário administrador
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- tests/requests.user.test.js
+++ tests/requests.user.test.js
@@ -41,3 +41,25 @@ test("POST /users deve falhar se o email já for cadastrado", async () => {
     expect(response.text).toContain("Email já cadastrado!");
 
 });
+
+test("POST /admin deve retornar um json com url de foto e nome de usuário", async () => {
+    const data = {
+        picture_profile_url: "foto",
+        username: "Teste 1",
+        email: "ficticio.exemplo2@gmail.com",
+        password: "123456"
+    }
+
+    const response = await request(app)
+                           .post("/users")
+                           .send(data)
+                           .set("Accept", "application/json")
+                           
+    
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("picture_profile_url");
+    expect(response.body).toHaveProperty("username");
+    expect(response.body.picture_profile_url).toBe(data.picture_profile_url);
+    expect(response.body.username).toBe(data.username);
+
+});
