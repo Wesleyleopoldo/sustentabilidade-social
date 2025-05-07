d611afbda4f1ac1ae2d11bd0d6189a97f81a1431
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-07 16:13:02 -0300

    Modifica função de tryCatch e cria outro metodo para consultas sql com parâmetros
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/helper/error.js
+++ src/helper/error.js
@@ -3,9 +3,20 @@ const defineColor = require("./colors");
 async function useTry(fun, errorMessage) {
     try {
         await fun();
-    } catch(error) {
+    } catch (error) {
         console.log(defineColor(`Algo deu errado: ${errorMessage} (${error})`, "red"));
     }
 }
 
-module.exports = useTry;
+async function tryQuery(errorMessage, fun, ...args) {
+    try {
+        return await fun(...args);
+    } catch(error) {
+        return console.log(defineColor(`Algo deu errado: ${errorMessage} (${error})`, "red"));
+    }
+}
+
+module.exports = {
+    useTry,
+    tryQuery
+};
