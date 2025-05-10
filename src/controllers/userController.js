ec89284603f7236f39f35471091f95afb926e40f
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-10 18:19:15 -0300

    Adiciona endpoint de atualizar nome de usuário e deletar usuário
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,11 +5,6 @@ const createUser = async (request, response) => {
     return response.status(201).json(newUser);
 }
 
-const createAdmin = async (request, response) => {
-    const newAdmin = await userService.createAdmin(request.body);
-    return response.status(201).json(newAdmin);
-}
-
 const indexAllUsers = async (_request, response) => {
     const allUsers = await userService.indexAllUsers();
     return response.status(200).json(allUsers);
@@ -20,9 +15,26 @@ const getUserById = async (request, response) => {
     return response.status(200).json(user);
 }
 
+const createAdmin = async (request, response) => {
+    const newAdmin = await userService.createAdmin(request.body);
+    return response.status(201).json(newAdmin);
+}
+
+const updateUsername = async (request, response) => {
+    const updatedUsername = await userService.updateUsername(request.params.id, request.body.username);
+    return response.status(200).json(updatedUsername);
+}
+
+const destroyUserById = async (request, response) => {
+    const deletedUser = await userService.destroyUserById(request.params.id);
+    return response.status(200).json(deletedUser);
+}
+
 module.exports = {
     createUser,
     indexAllUsers,
     getUserById,
-    createAdmin
+    createAdmin,
+    updateUsername,
+    destroyUserById
 }
