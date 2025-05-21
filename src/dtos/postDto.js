196ce2e1396fd648653f53450e791520ccd8dd37
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-21 19:15:44 -0300

    Adiciona dto de posts para futuras implementações de regras de negocios
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

---
+++ src/dtos/postDto.js
@@ -0,0 +1,21 @@
+function createPostDTO(data = {}) {
+    const {
+        id,
+        title,
+        content,
+        userId,
+        dateTime,
+        likes
+    } = data;
+
+    const dto = {};
+
+    if(id !== undefined) dto.id = id;
+    if(title !== undefined) dto.title = title;
+    if(content !== undefined) dto.content = content;
+    if(userId !== undefined) dto.userId = userId;
+    if(dateTime !== undefined) dto.dateTime = dateTime;
+    if(likes !== undefined) dto.likes = likes;
+
+    return dto;
+}
