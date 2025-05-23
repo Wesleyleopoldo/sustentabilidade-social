d3de7df9bba986586d607d2e65c6aa0cd97a42fb
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-23 17:21:09 -0300

    Adiciona rotas para criar e listar posts
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const userController = require("./controllers/userController");
+const postController = require("./controllers/postController");
 const router = express.Router();
 
+// Rotas para os recursos de usuários...
 router.post("/users", userController.createUser);
 router.get("/users", userController.indexAllUsers);
 router.get("/users/:id", userController.getUserById);
 router.post("/admin", userController.createAdmin);
 router.put("/users/:id/username", userController.updateUsername);
 router.put("/users/:id/email", userController.updateEmail);
-router.put("/users/:id/password", userController.updatePassword)
+router.put("/users/:id/password", userController.updatePassword);
 router.delete("/users/:id/delete", userController.destroyUserById);
+
+// Rotas para os recursos de posts...
+router.post("/:id/posts", postController.createPost);
+router.get("/posts", postController.indexAllPosts);
 
 module.exports = router;
