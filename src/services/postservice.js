2d7018b45242a838fedebe631565fbf1ab73a779
Qualquer coisa <Qualquer coisa>
2025-05-27 20:46:01 -0300

    Adiciona regras de negocios para criar, atualizar, excluir e listar comentários
    Signed-off-by: Qualquer coisa <Qualquer coisa>

--- src/services/postservice.js
+++ src/services/postservice.js
@@ -1,6 +1,7 @@
+const createCommentDTO = require("../dtos/commentDto");
 const createPostDTO = require("../dtos/postDto");
 const { tryQuery, useTry, AppError } = require("../helper/error");
-const { Post, User, Likes } = require("../resources/db");
+const { Post, User, Likes, Comments } = require("../resources/db");
 
 const createPost = async (title, content, userId, dateTime) => {
     const data = {
@@ -192,10 +193,113 @@ const removeLike = async (postId, userId) => {
     return likesDto;
 }
 
+const createComment = async (userId, postId, comment, createdAt) => {
+    const post = await tryQuery("Erro ao tentar buscar post!!!", () => Post.findByPk(postId));
+
+    if(!post) {
+        throw new AppError("Post não encontrado!!!", 404);
+    }
+
+    const findUser = await tryQuery("Erro ao tentar buscar usuário!!!", () => User.findByPk(userId));
+
+    if(!findUser) {
+        throw new AppError("Usuário não encontrado", 404);
+    }
+
+    const data = {
+        userId: findUser.id,
+        postId: post.id,
+        content: comment,
+        dateTime: createdAt
+    }
+
+    const newComment = await tryQuery("Erro ao tentar salvar comentário!!!", () => Comments.create(data));
+
+    const commentDTO = createCommentDTO({
+        id: newComment.id,
+        postId: post.id,
+        slug: findUser.slug,
+        userName: findUser.username,
+        content: newComment.content,
+        dateTime: newComment.dateTime
+    });
+
+    return commentDTO;
+}
+
+const updateComment = async (commentId, userId, content) => {
+    const comment = await tryQuery("Erro ao buscar comentário", () => Comments.findByPk(commentId));
+
+    if(!comment) {
+        throw new AppError("Comentário não encontrado!!!", 404);
+    }
+
+    const findUser = await tryQuery("Erro ao buscar usuário!!!", () => User.findByPk(userId));
+
+    if(!findUser) {
+        throw new AppError("Usuário não encontrado!!!", 404);
+    }
+
+    if(comment.userId !== userId) {
+        throw new AppError("Você não pode alterar o comentário de outro usuário", 401);
+    }
+
+    comment.content = content;
+
+    await comment.save();
+
+    const commentDTO = createCommentDTO({
+        id: comment.id,
+        slug: findUser.slug,
+        userName: findUser.username,
+        content: comment.content
+    });
+
+    return commentDTO;
+}
+
+const destroyComment = async (commentId, userId) => {
+    const comment = await tryQuery("Erro ao buscar comentário", () => Comments.findByPk(commentId));
+
+    if(!comment) {
+        throw new AppError("Comentário não encontrado!!!", 404);
+    }
+
+    const findUser = await tryQuery("Erro ao buscar usuário!!!", () => User.findByPk(userId));
+
+    if(!findUser) {
+        throw new AppError("Usuário não encontrado!!!", 404);
+    }
+
+    if(comment.userId !== userId) {
+        throw new AppError("Você não pode apagar o comentário de outro usuário", 401);
+    }
+
+    await tryQuery("Erro ao tentar apagar", () => Comments.destroy({
+        where: { id: comment.id }
+    }));
+
+    return "Sucesso!!!";
+}
+
+const indexAllCommentsByPostId = async (postId) => {
+    const post = await tryQuery("Erro ao tentar buscar post!!!", () => Post.findByPk(postId));
+
+    if(!post) {
+        throw new AppError("Post não encontrado!!!", 404);
+    }
+
+    const allComments = await tryQuery("Erro ao listar comentários!!", () => Comments.findAll({
+        where: { postId: post.id }
+    }));
+} 
+
 module.exports = {
     createPost,
     indexAllPosts,
     indexPost,
     addLike,
-    removeLike
+    removeLike,
+    createComment,
+    updateComment
 }
