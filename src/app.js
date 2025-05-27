e85599d3db0cff36c690b68703c8f4e9e08860b9
Wesley Cauan <wesleycauan01@gmail.com>
2025-05-27 17:25:01 -0300

    Adiciona o middleware de erros no app
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = require("./router");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.json());
-
 app.use(cors())
 
 app.use(router);
+app.use(errorHandler);
 
 module.exports = app;
