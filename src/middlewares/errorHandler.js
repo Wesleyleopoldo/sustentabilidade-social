aea4a9a3758a3925c6d860bcb6a3df4370c0fa5b
Qualquer coisa <Qualquer coisa>
2025-05-27 20:42:07 -0300

    Remove console.log dos testes de unidades
    Signed-off-by: Qualquer coisa <Qualquer coisa>

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,13 +3,10 @@ const { AppError } = require("../helper/error");
 function errorHandler(err, request, response, next) {
     const statusCode = err.statusCode || 500;
 
-    console.log(err.statusCode);
-
     response.status(statusCode).json({
         success: false,
         message: err.message || "Erro interno no servidor"
     });
-    console.log("Passou daqui");
 }
 
 module.exports = errorHandler
