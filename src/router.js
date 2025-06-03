60710f41bfb4b83735f38f8beb714d9f3adee5f1
Alunowes <alunoestudos3@gmail.com>
2025-06-03 20:40:10 -0300

    Adiciona validações
    Signed-off-by: Alunowes <alunoestudos3@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,4 +1,8 @@
 const express = require("express");
+<<<<<<< HEAD
+=======
+const validationToken = require("./middlewares/validationToken");
+>>>>>>> 68df98b (Adiciona validações falta muitoooooooooooooooooooooooooo)
 const userController = require("./controllers/userController");
 const postController = require("./controllers/postController");
 const router = express.Router();
@@ -6,9 +10,13 @@ const router = express.Router();
 // Rotas para os recursos de usuários...
 router.post("/users", userController.createUser);
 router.get("/users", userController.indexAllUsers);
+<<<<<<< HEAD
 router.get("/users/:id", userController.getUserById);
+=======
+router.get("/users", validationToken,userController.getUserById);
+>>>>>>> 68df98b (Adiciona validações falta muitoooooooooooooooooooooooooo)
 router.post("/admin", userController.createAdmin);
-router.put("/users/username", userMiddleware.verifyTokens, userController.updateUsername);
+router.put("/users/username", validationToken, userController.updateUsername);
 router.put("/users/:id/email", userController.updateEmail);
 router.put("/users/:id/password", userController.updatePassword);
 router.delete("/users/:id/delete", userController.destroyUserById);
