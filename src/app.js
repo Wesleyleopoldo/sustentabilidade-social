5a81a7a4a504f573fd999b1ee03bb471085e0345
Alunowes <alunoestudos3@gmail.com>
2025-06-04 20:37:24 -0300

    Adiciona documentação
    Signed-off-by: Alunowes <alunoestudos3@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+const fs = require("fs");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yaml");
 const express = require("express");
 const router = require("./router");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
+const swaggerDocument = YAML.parse(fs.readFileSync("./src/docs/swagger.yaml", "utf8"));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -11,5 +15,6 @@ app.use(cors())
 
 app.use(router);
 app.use(errorHandler);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
