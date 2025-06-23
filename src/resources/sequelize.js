a31e80240ba091c48b97e30a9284cb18bef8491d
Wesley Cauan <wesleycauan01@gmail.com>
2025-06-23 15:52:37 -0300

    Apaga antiga versão
    Signed-off-by: Wesley Cauan <wesleycauan01@gmail.com>

--- src/resources/sequelize.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize, DataTypes } = require("sequelize");
-
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./sustentabilidade_social.sql",
-    logging: false
-});
-
-module.exports = { Sequelize, DataTypes, sequelize }
