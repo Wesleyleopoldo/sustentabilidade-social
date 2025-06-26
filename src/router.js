a33daa171bf7db77ea6a75bc0a3547f7342e87c9
Your Name <you@example.com>
2025-06-26 19:29:07 -0300

    Adiciona parametros
    Signed-off-by: Your Name <you@example.com>

--- src/router.js
+++ src/router.js
@@ -8,6 +8,27 @@ const router = express.Router();
  * /users:
  *  post:
  *      summary: Rota para criar usuário.
+ *      tags: [Users]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     type: object
+ *                     properties:
+ *                         picture_profile_url:
+ *                             type: string
+ *                             format: binary
+ *                         username:
+ *                             type: string
+ *                             example: Silvio Monte
+ *                         email:
+ *                             type: string
+ *                             example: silvio.monte@gmail.com
+ *                         password:
+ *                             type: string
+ *                             example: senhaforte123
+ * 
  *      responses:
  *          201:
  *              description: Ela retorna foto e nome de usuário...
@@ -25,9 +46,17 @@ router.post("/users", userController.createUser);
 router.get("/users/allusers", userController.indexAllUsers);
 /**
  * @swagger
- * /:id/users:
+ * /{id}/users:
  *  get:
  *      summary: Rota para obter um usuário pelo id.
+ *      parameters:
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            description: UUID do usuário
+ *            schema:
+ *            type: string
+ *            format: uuid
  *      responses:
  *          200:
  *              description: Ela retorna id, foto de usuário, nome de usuário e email...
@@ -38,6 +67,26 @@ router.get("/:id/users", userController.getUserById);
  * /admin:
  *  post:
  *      summary: Rota para criar um usuário administrador.
+ *      tags: [Users]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     type: object
+ *                     properties:
+ *                         picture_profile_url:
+ *                             type: string
+ *                             format: binary
+ *                         username:
+ *                             type: string
+ *                             example: Silvio Monte
+ *                         email:
+ *                             type: string
+ *                             example: silvio.monte@gmail.com
+ *                         password:
+ *                             type: string
+ *                             example: senhaforte123
  *      responses:
  *          201:
  *              description: Ela retorna id, foto de usuário e nome de usuário...
@@ -45,9 +94,28 @@ router.get("/:id/users", userController.getUserById);
 router.post("/admin", userController.createAdmin);
 /**
  * @swagger
- * /users/:id/username:
+ * /users/{id}/username:
  *  put:
  *      summary: Rota para alterar nome de usuário pelo id.
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            description: UUID do usuário
+ *            schema:
+ *            type: string
+ *            format: uuid
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     type: object
+ *                     properties:
+ *                         username:
+ *                             type: string
+ *                             example: Silvio Monte
  *      responses:
  *          200:
  *              description: Ela retorna o novo nome de usuário...
@@ -55,9 +123,28 @@ router.post("/admin", userController.createAdmin);
 router.put("/users/:id/username", validationToken, userController.updateUsername);
 /**
  * @swagger
- * /users/:id/email:
+ * /users/{id}/email:
  *  put:
  *      summary: Rota para alterar o email do usuário pelo id.
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            description: UUID do usuário
+ *            schema:
+ *            type: string
+ *            format: uuid
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     type: object
+ *                     properties:
+ *                         email:
+ *                             type: string
+ *                             example: silvio.monte@gmail.com
  *      responses:
  *          200:
  *              description: Ela retorna o novo email do usuário...
@@ -65,9 +152,28 @@ router.put("/users/:id/username", validationToken, userController.updateUsername
 router.put("/users/:id/email", userController.updateEmail);
 /**
  * @swagger
- * /users/:id/password:
+ * /users/{id}/password:
  *  put:
  *      summary: Rota para alterar o email do usuário pelo id.
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            description: UUID do usuário
+ *            schema:
+ *            type: string
+ *            format: uuid
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     type: object
+ *                     properties:
+ *                         password:
+ *                             type: string
+ *                             example: senhaforte123
  *      responses:
  *          200:
  *              description: Ela retorna uma mensagem de sucesso para o usuário...
@@ -75,9 +181,18 @@ router.put("/users/:id/email", userController.updateEmail);
 router.put("/users/:id/password", userController.updatePassword);
 /**
  * @swagger
- * /users/:id/delete:
+ * /users/{id}/delete:
  *  put:
  *      summary: Rota para deletar usuário pelo id.
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: uuid
+ *            required: true
+ *            description: UUID do usuário
+ *            schema:
+ *            type: string
+ *            format: uuid
  *      responses:
  *          200:
  *              description: Ela retorna uma mensagem de sucesso para o usuário...
